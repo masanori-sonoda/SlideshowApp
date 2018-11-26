23d61994ea50f6eff2f7835dd9195d285f2753de
masanori.sonoda <masanori_sonoda@magocolo-design.com>
2018-11-26 10:18:48 +0900

    画像表示OK

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -12,12 +12,20 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
     
-    // バンドルした画像ファイルを読み込み
-    let image = UIImage(named: "01")
+    @IBAction func prevImage(_ sender: Any) {
+    }
+    
+    @IBAction func nextImage(_ sender: Any) {
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // 画像ファイルの読み込み
+        let image = UIImage(named: "01.jpg")
+        
+        // Image Viewに画像を設定
+        imageView.image = image
     }
 
     override func didReceiveMemoryWarning() {
